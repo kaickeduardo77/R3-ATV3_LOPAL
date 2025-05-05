e91f0f31c22e24a0dc0668950023dbe3efff11a2
kaickeduardo77 <kaickeduardo415@gmail.com>
2025-05-05 15:55:33 -0300

    Delete Exercicio 5/atividade5.js

--- Exercicio 5/atividade5.js
+++
@@ -1,6 +0,0 @@
-let temperaturaCelsius=parseFloat(prompt("insira a temperaturaCelsius"))//apresentaçao da variavel temperatura em Celsius
-
-let grausFahrenheit= ((temperaturaCelsius*9)/5) +32; //o valor convertido da variaveltemperaturaCelsius trasnformada em Fahrenheit
-
-
-alert(`a temperatura convertida em fahrenheit é:${grausFahrenheit.toFixed(2)}`);
